22cd9471829fd70bdc202ddd63eeb1ecae578f61
jharmelink <jharmelink@gmail.com>
2014-02-06 21:20:51 +0100

    correct event type

--- serial-reader/src/main/java/net/harmelink/powerio/SerialEventListener.java
+++ serial-reader/src/main/java/net/harmelink/powerio/SerialEventListener.java
@@ -13,6 +13,8 @@ import java.io.IOException;
 public class SerialEventListener implements SerialPortEventListener {
     private static final Logger LOG = LoggerFactory.getLogger(SerialEventListener.class);
 
+    private static final int BUFFER = 20;
+
     private final SerialPort serialPort;
 
     private final Writer writer;
@@ -23,23 +25,19 @@ public class SerialEventListener implements SerialPortEventListener {
     }
 
     public void serialEvent(final SerialPortEvent event) {
-        LOG.debug("Status: {} - {}", event.getEventType(), event.getEventValue());
-
-        if (event.isDSR()) { // If data is available
-            if (event.getEventValue() == 10) { // Check bytes count in the input buffer
-                try {
-                    byte buffer[] = serialPort.readBytes(10); // Read data, if 10 bytes available
-                    writer.write(buffer);
-                } catch (final SerialPortException e) {
-                    LOG.error("Unable to read from serial port: {}", e.getMessage());
-                } catch (final IOException e) {
-                    LOG.error("Unable to serialize serial data: {}", e.getMessage());
-                }
+        if (event.isRXCHAR() && event.getEventValue() >= BUFFER) {
+            try {
+                byte buffer[] = serialPort.readBytes(event.getEventValue());
+                writer.write(buffer);
+            } catch (final SerialPortException e) {
+                LOG.error("Unable to read from serial port: {}", e.getMessage());
+            } catch (final IOException e) {
+                LOG.error("Unable to serialize serial data: {}", e.getMessage());
             }
         } else if (event.isCTS()) {
             LOG.debug("CTS - {}", event.getEventValue() == 1 ? "ON" : "OFF");
         } else if (event.isDSR()) {
-            LOG.debug("DSR - ON{}", event.getEventValue() == 1 ? "ON" : "OFF");
+            LOG.debug("DSR - {}", event.getEventValue() == 1 ? "ON" : "OFF");
         }
     }
 }
